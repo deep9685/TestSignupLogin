5e10158fb895062b57e9370352b2cfb1d0c6e5c8
deep9685 <deepaknmdv14@gmail.com>
2024-06-27 15:04:45 +0530

    update cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 
 
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
@@ -36,7 +37,7 @@ testDbConnection();
 const SignupRoute = require('./routes/SignupRoute');
 const loginRoute = require('./routes/loginRoute');
 
-// app.use(cors());
+app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cookieParser());
