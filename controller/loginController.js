6f7e2a8001836ad5fc630297079e3f51bd4e8b0e
deep9685 <deepaknmdv14@gmail.com>
2024-07-05 22:37:01 +0530

    file data

--- controller/loginController.js
+++ controller/loginController.js
@@ -92,10 +92,10 @@ async function handleFileUpload(req, res) {
     const files = req.files;
 
     const data = req.body;
-    
-    console.log(data);
 
-    console.log(files);
+    console.log("Json data : " ,data);
+
+    console.log("Files data : " ,files);
 
     const fileType = req.params.type;
 
